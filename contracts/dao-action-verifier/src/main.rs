9a4a7e50ca7cba87a227b0f5e702fb482ffec45a
ian <ian@cryptape.com>
2024-02-01 10:51:29 +0800

    :sparkles: Verify deposit output exists

--- contracts/dao-action-verifier/src/main.rs
+++ contracts/dao-action-verifier/src/main.rs
@@ -11,15 +11,18 @@ ckb_std::entry!(program_entry);
 #[cfg(not(test))]
 default_alloc!();
 
-use ckb_dao_cobuild_schemas::DaoActionDataReader;
-use ckb_std::ckb_types::prelude::*;
+use ckb_dao_cobuild_schemas::{DaoActionData, DaoActionDataReader};
+use ckb_std::ckb_types::{bytes::Bytes, prelude::*};
 use ckb_transaction_cobuild::fetch_message;
 
 mod constants;
+mod derived_dao_action_data;
 mod error;
 mod error_code;
 
-use crate::{constants::DAO_SCRIPT_HASH, error::Error};
+use crate::{
+    constants::DAO_SCRIPT_HASH, derived_dao_action_data::DerivedDaoActionData, error::Error,
+};
 
 pub fn program_entry() -> i8 {
     match verify() {
@@ -30,18 +33,22 @@ pub fn program_entry() -> i8 {
 
 fn verify() -> Result<(), Error> {
     if let Ok(Some(message)) = fetch_message() {
+        let mut derived_dao_action_data = DerivedDaoActionData::derive();
+
         for action in message.actions().into_iter() {
             if action.script_hash().as_slice() == DAO_SCRIPT_HASH {
-                verify_action_data(&action.data().raw_data())?;
+                let dao_action_data = decode_dao_action_data(action.data().raw_data())?;
+                derived_dao_action_data.verify(dao_action_data)?;
             }
         }
-    }
 
-    // It's OK to not include DAO action data
-    Ok(())
+        derived_dao_action_data.complete()
+    } else {
+        Ok(())
+    }
 }
 
-fn verify_action_data(data: &[u8]) -> Result<(), Error> {
-    DaoActionDataReader::from_slice(data).map_err(|err| trace_error!(err))?;
-    Ok(())
+fn decode_dao_action_data(data: Bytes) -> Result<DaoActionData, Error> {
+    DaoActionDataReader::from_slice(&data).map_err(|err| trace_error!(err))?;
+    Ok(DaoActionData::new_unchecked(data))
 }
